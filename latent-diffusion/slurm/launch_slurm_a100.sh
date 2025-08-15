8695e26f81d5c73b8d2876b2a06851138d5e0f0a
adriarm <adria.rodriguez.munoz@gmail.com>
2025-08-15 09:48:43 -0400

    rename edm2 to latent-diffusion for consistency

---
+++ latent-diffusion/slurm/launch_slurm_a100.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+full_name=$1
+last_two=$(echo "$full_name" | rev | cut -d'/' -f1,2 | rev)
+b=$(echo "$last_two" | rev | cut -d'/' -f1 | rev)
+a=$(echo "$last_two" | rev | cut -d'/' -f2 | rev)
+xpref=${a}_${b}
+
+job_name=$xpref
+echo job_name=$job_name
+
+sbatch --requeue <<EOT
+#!/bin/bash
+#SBATCH --partition=csail-shared
+#SBATCH --qos=lab-free
+#SBATCH --nodes=1
+#SBATCH --time=1-00:00:00
+#SBATCH --gpus-per-node=a100:8
+#SBATCH --exclude=agrawal-v100-1
+#SBATCH --cpus-per-task=80
+#SBATCH --mem=500G
+#SBATCH --job-name=$job_name
+#SBATCH --output=./slurm_logs/${job_name}/%j.out
+
+$1
+EOT
---
+++ latent-diffusion/slurm/launch_slurm_torralba_3090.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+full_name=$1
+last_two=$(echo "$full_name" | rev | cut -d'/' -f1,2 | rev)
+b=$(echo "$last_two" | rev | cut -d'/' -f1 | rev)
+a=$(echo "$last_two" | rev | cut -d'/' -f2 | rev)
+xpref=${a}_${b}
+
+job_name=$xpref
+echo job_name=$job_name
+
+sbatch --requeue <<EOT
+#!/bin/bash
+#SBATCH --partition=vision-torralba
+#SBATCH --qos=vision-torralba-main
+#SBATCH --nodes=1
+#SBATCH --time=3-00:00:00
+#SBATCH --nodelist=torralba-3090-[1-3]
+#SBATCH --gpus-per-node=7
+#SBATCH --exclude=agrawal-v100-1
+#SBATCH --cpus-per-task=80
+#SBATCH --mem=200G
+#SBATCH --job-name=$job_name
+#SBATCH --output=./slurm_logs/${job_name}/%j.out
+
+$1
+EOT
